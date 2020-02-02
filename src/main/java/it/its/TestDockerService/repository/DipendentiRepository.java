fea16772bf496c3aa9dadaf6945bcb476c8eacf9
ITSManucA <ITSManucA@itscor1911.itsacademy.corvallisholding.it>
2020-02-02 13:28:04 +0100

    tolto un commento

--- src/main/java/it/its/TestDockerService/repository/DipendentiRepository.java
+++ src/main/java/it/its/TestDockerService/repository/DipendentiRepository.java
@@ -6,7 +6,4 @@ import it.its.TestDockerService.dao.DipendentiDao;
 
 public interface DipendentiRepository extends JpaRepository<DipendentiDao, Long>{ //DipendentiDao (entità) e identificativo(Long)
 
-//	Promo findByIdPromo(String IdPromo);
-	//sull'esempio degli articoli nella sezione promo c'era questo su Repository per quanto riguarda il select
-	//per quanto riguarda il delete non c'era niente
 }
