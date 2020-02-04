08526fe28af650eb297e8bead26acadb222a4fcd
ITSMarchioroM <ITSMarchioroM@itscor1918.itsacademy.corvallisholding.it>
2020-02-04 15:38:49 +0100

    cross origin

--- src/main/java/it/its/TestDockerService/controller/DipendentiController.java
+++ src/main/java/it/its/TestDockerService/controller/DipendentiController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +28,7 @@ import it.its.TestDockerService.dao.DipendentiDao;
 import it.its.TestDockerService.dto.BaseResponseDto;
 import it.its.TestDockerService.dto.DipendentiDto;
 import it.its.TestDockerService.service.DipendentiService;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "api/dipendenti")
 //pushatelo raga
