3b0240b38b73cdcd093abf43f15deeae42c16d50
ITSFerroC <ITSFerroC@itscor1908.itsacademy.corvallisholding.it>
2020-03-16 17:37:52 +0100

    cors

--- src/main/java/it/its/TestDockerService/controller/AuthController.java
+++ src/main/java/it/its/TestDockerService/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,7 @@ import it.its.TestDockerService.repository.UtentiRepository;
 import it.its.TestDockerService.security.JwtTokenProvider;
 
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
